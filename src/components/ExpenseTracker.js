8d32a641d782ab732ff0710edb6566d34bf9dfd5
ujjwalsingh996 <ujjwalsingh996@gmail.com>
2023-03-27 15:22:23 +0530

    saving the expenses to the database

--- src/components/ExpenseTracker.js
+++ src/components/ExpenseTracker.js
@@ -1,80 +1,132 @@
-import React, { useContext, useRef } from "react";
-import './ExpenseTracker.css'
+import React, { useContext, useEffect, useRef, useState } from "react";
+import "./ExpenseTracker.css";
 import { useHistory } from "react-router-dom";
 import { Link } from "react-router-dom";
 import AuthContext from "../store/auth-context";
+import axios from "axios";
 
 const ExpenseTracker = () => {
-    const moneyInputRef = useRef();
-    const descInputRef = useRef();
-    const categoryInputRef = useRef();
-    const authCtx = useContext(AuthContext)
-    const history = useHistory();
+  const [expenses, setExpenses] = useState([]);
+  const moneyInputRef = useRef();
+  const descInputRef = useRef();
+  const categoryInputRef = useRef();
+  const authCtx = useContext(AuthContext);
+  const history = useHistory();
 
-    const verifyEmailHandler = async() => {
-        try{
-            const response = await fetch('https://identitytoolkit.googleapis.com/v1/accounts:sendOobCode?key=AIzaSyDCbHcNqtDAJHrL7U_2YgYvyOjHTc60FoA', {
-            method: 'POST',
-            body: JSON.stringify({
-                requestType: "VERIFY_EMAIL",
-                idToken: authCtx.token,
-            }),
-            headers: {
-                "Content-Type": "application/json",
-              },    
-
-        })
-        let data;
-        if(response.ok)
+  const verifyEmailHandler = async () => {
+    try {
+      const response = await fetch(
+        "https://identitytoolkit.googleapis.com/v1/accounts:sendOobCode?key=AIzaSyDCbHcNqtDAJHrL7U_2YgYvyOjHTc60FoA",
         {
-            data = await response.json()
-            console.log('Email ID verified')
-        } else {
-            data = await response.json();
-            let errorMessage = 'Verification Failed'
-            throw new Error(errorMessage)
+          method: "POST",
+          body: JSON.stringify({
+            requestType: "VERIFY_EMAIL",
+            idToken: authCtx.token,
+          }),
+          headers: {
+            "Content-Type": "application/json",
+          },
         }
+      );
+      let data;
+      if (response.ok) {
+        data = await response.json();
+        console.log("Email ID verified");
+      } else {
+        data = await response.json();
+        let errorMessage = "Verification Failed";
+        throw new Error(errorMessage);
+      }
     } catch (err) {
-        console.log(err)
+      console.log(err);
     }
-    }   
-    const logOutHandler = () => {
-        authCtx.logout();
-        history.replace('/login')
-    }
+  };
+  const logOutHandler = () => {
+    authCtx.logout();
+    history.replace("/login");
+  };
 
-    const expenseHandler = (event) => {
-        event.preventDefault();
-        const enteredMoney = moneyInputRef.current.value;
-        const enteredDesc = descInputRef.current.value;
-        const enteredCategory = categoryInputRef.current.value;
-        console.log(enteredMoney, enteredDesc, enteredCategory)
-        
+  const expenseHandler = async (event) => {
+    event.preventDefault();
+    const enteredMoney = moneyInputRef.current.value;
+    const enteredDesc = descInputRef.current.value;
+    const enteredCategory = categoryInputRef.current.value;
+    console.log(enteredMoney, enteredDesc, enteredCategory);
+    const obj = {
+      money: enteredMoney,
+      desc: enteredDesc,
+      category: enteredCategory,
+    };
 
-        moneyInputRef.current.value = '';
-        descInputRef.current.value = '';
-        categoryInputRef.current.value = ''
-    }
-    return(<React.Fragment>
-        <div className="right"> <button onClick={logOutHandler}>Logout</button></div><h1 className="header">Welcome to ExpenseTracker!</h1>
-    <button className="button2" onClick={verifyEmailHandler}>Verify Email Id</button>
-    <h3 className="header"><Link to="/update">Your Profile is Incomplete</Link></h3>
-    <form className="input" onSubmit={expenseHandler}>
+    moneyInputRef.current.value = "";
+    descInputRef.current.value = "";
+    categoryInputRef.current.value = "";
+    axios
+      .post(
+        "https://expense-tracker-1266e-default-rtdb.firebaseio.com/expenses.json",
+        obj
+      )
+      .then((res) => console.log(res.data));
+
+    axios
+      .get(
+        "https://expense-tracker-1266e-default-rtdb.firebaseio.com/expenses.json"
+      )
+      .then((response) => {
+        console.log(response.data);
+        setExpenses(response.data);
+        console.log(expenses);
+      });
+  };
+  useEffect(() => {
+    axios
+      .get(
+        "https://expense-tracker-1266e-default-rtdb.firebaseio.com/expenses.json"
+      )
+      .then((response) => {
+        console.log(response.data);
+        setExpenses(response.data);
+        console.log(expenses);
+      });
+  }, []);
+  return (
+    <React.Fragment>
+      <div className="right">
+        {" "}
+        <button onClick={logOutHandler}>Logout</button>
+      </div>
+      <h1 className="header">Welcome to ExpenseTracker!</h1>
+      <button className="button2" onClick={verifyEmailHandler}>
+        Verify Email Id
+      </button>
+      <h3 className="header">
+        <Link to="/update">Your Profile is Incomplete</Link>
+      </h3>
+      <form className="input" onSubmit={expenseHandler}>
         <label>Money Spent:</label>
         <input type="number" ref={moneyInputRef} required></input>
         <label>Description:</label>
         <input type="text" ref={descInputRef} required></input>
         <label>Category:</label>
         <select ref={categoryInputRef}>
-            <option value="food">Food</option>
-            <option value="fuel">Fuel</option>
-            <option value="salary">Salary</option>
+          <option value="food">Food</option>
+          <option value="fuel">Fuel</option>
+          <option value="salary">Salary</option>
         </select>
         <button type="submit">Add Expense</button>
-    </form>
-   
-    </React.Fragment>)
-
-}
+      </form>
+      <ul>
+        {Object.keys(expenses).map((key) => (
+          <ul className="header" key={key}>
+            <h3>
+              Rs {expenses[key].money} ; Category - {expenses[key].category}{" "}
+              ;Description - {expenses[key].desc}
+            </h3>
+          </ul>
+        ))}
+      </ul>
+    </React.Fragment>
+  );
+};
 
 export default ExpenseTracker;
