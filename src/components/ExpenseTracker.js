1971b2808b59d5cd28146aca454e7ab7d679d269
ujjwalsingh996 <ujjwalsingh996@gmail.com>
2023-03-27 12:43:13 +0530

    email verification

--- src/components/ExpenseTracker.js
+++ src/components/ExpenseTracker.js
@@ -1,8 +1,39 @@
-import React from "react";
+import React, { useContext } from "react";
 import { Link } from "react-router-dom";
+import AuthContext from "../store/auth-context";
 
 const ExpenseTracker = () => {
-    return(<React.Fragment><h1>Welcome to ExpenseTracker!</h1>
+    const authCtx = useContext(AuthContext)
+
+    const verifyEmailHandler = async() => {
+        try{
+            const response = await fetch('https://identitytoolkit.googleapis.com/v1/accounts:sendOobCode?key=AIzaSyDCbHcNqtDAJHrL7U_2YgYvyOjHTc60FoA', {
+            method: 'POST',
+            body: JSON.stringify({
+                requestType: "VERIFY_EMAIL",
+                idToken: authCtx.token,
+            }),
+            headers: {
+                "Content-Type": "application/json",
+              },    
+
+        })
+        let data;
+        if(response.ok)
+        {
+            data = await response.json()
+            console.log('Email ID verified')
+        } else {
+            data = await response.json();
+            let errorMessage = 'Verification Failed'
+            throw new Error(errorMessage)
+        }
+    } catch (err) {
+        console.log(err)
+    }
+    }   
+    return(<React.Fragment><h1 className="header">Welcome to ExpenseTracker!</h1>
+    <button className="button2" onClick={verifyEmailHandler}>Verify Email Id</button>
     <Link to="/update">Your profile is Incomplete</Link></React.Fragment>)
 }
 
